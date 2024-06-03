f4088ab14d2ddc68e70a798ed8fedfd98254a361
Arvind Vishwakarma <arvindvishwakrma37@gmail.com>
2024-06-04 02:07:18 +0530

    test

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,8 +10,8 @@ function Cards() {
             Transactions
           </h1>
         </div>
-        <div className="flex  gap-5 justify-around max-lg:flex-col items-center ">
-          <div className="card-container bg-zinc-100 ml-5 max-lg:ml-0 rounded-md relative max-lg:flex max-lg:items-end max-lg:size-96  hover:scale-110 hover:border-2 border-zinc-700  hover:ml-10 ease-in-out transition duration-500 hover:z w-full md:w-[35vw] h-[55vh] hover:z-20">
+        <div className="flex  gap-5 justify-around max-lg:flex-col max-lg:mb-5 items-center ">
+          <div className="card-container bg-zinc-100 ml-5 max-lg:ml-0 rounded-md relative max-lg:flex max-lg:items-end max-lg:size-96  hover:scale-110 hover:border-2 border-zinc-700  hover:ml-10 ease-in-out transition duration-500 hover:z w-full md:w-[35vw] h-[55vh] hover:z-20 max-sm:hover:scale-100 max-sm:hover:border-none">
             <span className="text-black top-2 left-2 bg-[#FFEDE8] py-7 text-2xl shadow-2xl border-zinc-300 border z-20 rounded-full px-2 font-bold absolute">
               Step 1
             </span>
@@ -21,24 +21,24 @@ function Cards() {
               alt="Application-guide"
             />
           </div>
-          <div className="card-container bg-zinc-100 hover:scale-125 max-lg:flex max-lg:items-end max-lg:size-96 hover:border-2 border-zinc-700 ease-in-out transition duration-500 hover:z-20 rounded-md relative w-full md:w-[35vw] h-[55vh]">
+          <div className="card-container bg-zinc-100 hover:scale-125 max-lg:flex max-lg:items-end max-lg:size-96 hover:border-2 border-zinc-700 ease-in-out transition duration-500 hover:z-20 max-sm:hover:scale-100 max-sm:hover:border-none rounded-md relative w-full md:w-[35vw] h-[55vh]">
             <span className="text-black top-2 left-2 bg-[#FFF6E9] shadow-2xl border-zinc-300 border  py-7 text-2xl z-20 rounded-full px-2 font-bold absolute">
               Step 2
             </span>
             <img
               className="w-full rounded-lg h-[40vh] absolute max-lg:static bottom-0 right-0"
               src="https://www.benow.in/assets/img/app2.png"
-              alt="Application-guide"
+              alt="Login-guide"
             />
           </div>
-          <div className="card-container bg-zinc-100 rounded-md hover:border-2 max-lg:flex max-lg:items-end max-lg:size-96  border-zinc-700 hover:scale-110 ease-in-out transition duration-500 hover:z-20 hover:right-10 relative w-full md:w-[35vw] h-[55vh]">
-            <span className="text-black top-2 left-2 bg-[rgb(237,255,223)] py-7 shadow-2xl border-zinc-300 border text-2xl z-20 rounded-full px-2 font-bold absolute">
+          <div className="card-container bg-zinc-100 rounded-md hover:border-2 max-lg:flex max-lg:items-end max-lg:size-96  border-zinc-700 hover:scale-110 ease-in-out transition duration-500 hover:z-20 max-sm:hover:scale-100 max-sm:hover:border-none hover:right-10 relative w-full md:w-[35vw] h-[55vh]">
+            <span className="text-black top-2 left-2 bg-[rgb(237,255,223)] py-7 shadow-2xl border-zinc-300 border text-2xl  rounded-full px-2 font-bold absolute">
               Step 3
             </span>
             <img
               className="w-full rounded-lg h-[40vh] absolute max-lg:static bottom-0 right-0"
               src="https://www.benow.in/assets/img/app3.png"
-              alt="Application-guide"
+              alt="KYC-guide"
             />
           </div>
         </div>
